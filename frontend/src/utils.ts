a9b5bedde75f7140e0c7d684b2a901c788009bbb
icedoutskay <somkeneodikpo@gmail.com>
2024-08-15 18:05:20 +0100

    first commit

---
+++ frontend/src/utils.ts
@@ -0,0 +1,9 @@
+// const Config = {
+// 	API_URL: "http://localhost:5000/api",
+// 	BASE_URL: "http://localhost:5000",
+// };
+const Config = {
+	API_URL: "https://icedoutskay.tech/api",
+	BASE_URL: "https://icedoutskay.tech",
+};
+export default Config;
